7ac9e8f32ea3a810ba3b1b6451367515fc5a1828
manisha <chennojumaneesha@gmail.com>
2023-04-27 19:56:12 +0000

    solution

---
+++ src/components/GradientDirectionItem/index.js
@@ -0,0 +1,23 @@
+import {DirectionItem, DirectionLi} from './styledComponents'
+
+const GradientDirectionItem = props => {
+  const {details, onClickChangeDirection, isActive} = props
+  const {directionId, value, displayText} = details
+  const onClickToggleButton = () => {
+    console.log('----------onClickToggleButton-----------')
+    onClickChangeDirection(directionId, value)
+  }
+  return (
+    <DirectionLi key={directionId}>
+      <DirectionItem
+        active={isActive}
+        value={value}
+        onClick={onClickToggleButton}
+      >
+        {displayText}
+      </DirectionItem>
+    </DirectionLi>
+  )
+}
+
+export default GradientDirectionItem
